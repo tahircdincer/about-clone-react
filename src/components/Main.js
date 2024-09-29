abb70233b58197ca4e4bc5faf6bdb9e8daede91c
Tahir Dincer <tahircoskunerdincer@gmail.com>
2024-09-29 19:19:32 +0300

    ' First ' commit. ' Initiated ' React ' '. Built ' Navbar, Main ' components. Implementing CSS to these components.

---
+++ src/components/Main.js
@@ -0,0 +1,8 @@
+export default function Main() {
+  return (
+    <main>
+      <h2>How can we help?</h2>
+      <input type="text" placeholder="Search" className="main__search" />
+    </main>
+  );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar__logo--area">
+        <h1>Abstract</h1>
+        <h3>Help Center</h3>
+      </div>
+      <div className="navbar__button--area">
+        <button className="btn__request">Submit a request</button>
+        <button className="btn__sign--in">Sign in</button>
+      </div>
+    </nav>
+  );
+}
